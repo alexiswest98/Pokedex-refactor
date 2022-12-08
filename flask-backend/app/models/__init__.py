70ae42adafcf892d455dea666727c75e2dfec780
Alexis West <alexishwest98@gmail.com>
2022-12-08 08:58:30 -0800

    Started models

--- flask-backend/app/models/__init__.py
+++ flask-backend/app/models/__init__.py
@@ -1,3 +1,28 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+types = [
+  "fire",
+  "electric",
+  "normal",
+  "ghost",
+  "psychic",
+  "water",
+  "bug",
+  "dragon",
+  "grass",
+  "fighting",
+  "ice",
+  "flying",
+  "poison",
+  "ground",
+  "rock",
+  "steel",
+]
+
+class Item(db.Model):
+    __tablename__ = 'items'
+
+class Pokemon(db.Model):
+    __tablename__ = 'pokemons'
