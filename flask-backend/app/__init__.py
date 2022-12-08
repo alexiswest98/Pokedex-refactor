e61b7a5646eb839228aa902436ed6c0d06e6de8b
Alexis West <alexishwest98@gmail.com>
2022-12-08 08:44:23 -0800

    Added boilerplate

--- flask-backend/app/__init__.py
+++ flask-backend/app/__init__.py
@@ -1,8 +1,18 @@
-
+from flask import Flask, render_template, redirect 
 # import statement for CSRF
 from flask_wtf.csrf import CSRFProtect, generate_csrf
+from .models import db
+from flask_migrate import Migrate
 
+#boilerplate
+app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
+Migrate(app, db)
 
+#example blueprints 
+#app.register_blueprint(jokes_routes)
+#app.register_blueprint(users_routes, url_prefix="/users")
 
 # after request code for CSRF token injection
 @app.after_request
@@ -15,3 +25,5 @@ def inject_csrf_token(response):
             'FLASK_ENV') == 'production' else None,
         httponly=True)
     return response
+
+#start here ...
