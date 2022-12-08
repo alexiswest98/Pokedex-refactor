1965d71d6e18a6bbdb38e98500e83f1ef72c2dc4
michaellacey <mikelacey123123@gmail.com>
2022-12-08 12:40:17 -0500

    asdf

--- flask-backend/app/models/__init__.py
+++ flask-backend/app/models/__init__.py
@@ -1,10 +1,14 @@
-from flask_sqlalchemy import SQLAlchemy
 
-
-
-db = SQLAlchemy()
-
-
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
+class Pokemon(db.Model):
+    __tablename__ = 'pokemons'
+    id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.Integer, unique=True,  nullable=False)
+    attack = db.Column(db.Integer, nullable=False)
+    defense = db.Column(db.Integer, nullable=False)
+    image_url = db.Column(db.String(1000), nullable=False)
+    name = db.Column(db.String(1000), nullable=False, unique=True)
+    type = db.Column()
+    moves = db.Column(db.String(255), nullable=False)
+    encounter_rate = db.Column(db.Float)
+    catch_rate = db.Column(db.Float)
+    captured = db.Column(db.Boolean)  #no validations
