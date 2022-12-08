6b96176de72729bddce0e32ae3f400bec785003d
michaellacey <mikelacey123123@gmail.com>
2022-12-08 12:40:42 -0500

    asdfasdf

--- flask-backend/app/models/__init__.py
+++ flask-backend/app/models/__init__.py
@@ -1,3 +1,41 @@
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+db = SQLAlchemy()
+
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+types = [
+  "fire",
+  "electric",
+  "normal",
+  "ghost",
+  "psychic",
+  "water",
+  "bug",
+  "dragon",
+  "grass",
+  "fighting",
+  "ice",
+  "flying",
+  "poison",
+  "ground",
+  "rock",
+  "steel",
+]
+
+class Item(db.Model):
+    __tablename__ = 'items'
+    id = db.Column(db.Integer, primary_key=True) #no validations
+    happiness = db.Column(db.Integer) #no validations
+    image_url = db.Column(db.String(1000), nullable=False)
+    name = db.Column(db.String(1000), nullable=False)
+    price = db.Column(db.Integer, nullable=False)  #no validations
+    pokemon_id = db.Column(db.Integer, nullable=False)  #no validations
 
 class Pokemon(db.Model):
     __tablename__ = 'pokemons'
